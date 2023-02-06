058750da89f437e525350754ab3fefbf7919bfa8
helloyanis <40769776+helloyanis@users.noreply.github.com>
2023-02-06 08:02:27 +0100

    Update about.rs

--- prpr-client/src/page/about.rs
+++ prpr-client/src/page/about.rs
@@ -14,12 +14,12 @@ impl AboutPage {
             scroll: Scroll::new(),
             text: format!(
                 r"prpr-client v{}
-prpr 是一款 Phigros 模拟器，旨在为自制谱游玩提供一个统一化的平台。请自觉遵守社群相关要求，不恶意使用 prpr，不随意制作或传播低质量作品。
+prpr is a Phigros simulator designed to provide a unified platform for homemade play. Please consciously abide by the relevant requirements of the community, do not use PRPR maliciously, and do not arbitrarily produce or disseminate low-quality works.
 
-本软件使用的默认材质皮肤（包括音符材质和打击特效）来自于 @MisaLiu 的 phi-chart-render（https://github.com/MisaLiu/phi-chart-render），在 CC BY-NC 4.0 协议（https://creativecommons.org/licenses/by-nc/4.0/）下署名。在本软件的开发过程中，这些材质被调整尺寸并压缩以便使用。
+The default Material Skins used in this software (including note materials and percussion effects) are derived from @MisaLiu's phi-chart-render (https://github.com/MisaLiu/phi-chart-render), signed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by-nc/4.0/). During the development of this software, these materials were resized and compressed for use.
 
-prpr 是开源软件，遵循 GNU General Public License v3.0 协议。
-测试群：660488396
+prpr is open source software under the GNU General Public License v3.0.
+Test Group：660488396
 GitHub: https://github.com/Mivik/prpr",
                 env!("CARGO_PKG_VERSION")
             ),
@@ -29,7 +29,7 @@ GitHub: https://github.com/Mivik/prpr",
 
 impl Page for AboutPage {
     fn label(&self) -> &'static str {
-        "关于"
+        "About"
     }
 
     fn update(&mut self, _focus: bool, state: &mut SharedState) -> Result<()> {
